4598e7376d011c89fddfb13963d4b7cc8710cb68
piperlaney123 <piperddehoyos@gmail.com>
2024-04-03 10:05:59 -0600

    created models, installed bootstrap

--- game_review/django_project/settings.py
+++ game_review/django_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'game_review_app'
+
+    'game_review_app',
+    'taggit'
+    'django_bootstrap5'
 
 ]
 
--- game_review/game_review_app/models.py
+++ game_review/game_review_app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
+
+class User(models.Model):
+
+    name = models.CharField(max_length=200)
+    about = models.TextField(blank = True)
+    is_active_user = models.BooleanField(default = False)
+    preferred_game_genres = TaggableManager()
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("user_detail", args=[str(self.id)])
+
+class Review(models.Model):
+
+    game_title = models.CharField(max_length=200)
+    # List of choices for major in database, human readable name
+    '''STAR_RATING = (
+        ('1', 'One'),
+        ('2', 'Two'),
+        ('3', 'Three'),
+        ('4', 'Four'),
+        ('5', 'Five')
+    )
+
+    star_rating = models.CharField(max_length=200, choices=STAR_RATING)'''
+    tags = TaggableManager()
+    review_summary = models.TextField(blank = True)
+    would_recommend = models.BooleanField(default = False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.game_title
+    
+    def get_absolute_url(self):
+        return reverse("review_detail", args=[str(self.id)])
+    
+
+class Rating(models.Model):
+    review = models.OneToOneField(Review, null = True, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.review.game_title}"
+    
